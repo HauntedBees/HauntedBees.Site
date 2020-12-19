b7bb64150b3f539a3e988df81480bae9806f9f54
Sean Finch <fench@hauntedbees.com>
2020-12-19 13:40:52 -0800

    verbage changes

--- src/views/SaveTheBees.vue
+++ src/views/SaveTheBees.vue
@@ -8,15 +8,11 @@
     </p>
     <links :content="links" />
     <p class="mx-1">
-        <strong>Disclaimer:</strong> Since this apparently needs to be said, <strong>please do not contact us to share your business's singular blog post about saving bees,
-        we are not here to advertise for you or boost your site's SEO.</strong> In the past, several people have reached out to us and said this page was helpful to them, 
-        and shared additional links to other helpful resources. While we appreciated those emails and still do, this page is first and foremost a helpful 
-        resource for helping bees and educating people about them, so we will gladly promote any bee-related organizations, charities, and educational
-        resources, but we will not link to any pages that belong to websites promoting for-profit products or services. Haunted Bees Productions has no relation to
-        any of the listed sites above and has not been compensated for their presence here. If you are a representative of any of the organizations or websites listed 
-        here and want to be removed from this list, contact us at <a href="mailto:fench@hauntedbees.com">fench@hauntedbees.com</a>. Likewise, if you are <em>not</em> 
-        a representative of any of the above organizations but think they actually do more harm than good, let us know so we can remove them! And, of course, if you are 
-        a charitable organization whose primary goal is to help bees, contact us if you want to be listed here.
+        <strong>Disclaimer:</strong> Since this apparently needs to be said, <strong>please do not contact us to share your business's
+        singular blog post about saving bees, we are not here to advertise for you or boost your site's SEO.</strong> While we will
+        gladly promote any bee-related organizations, charities, and educational resources, we will NOT link to websites promoting
+        for-profit products or services. Haunted Bees Productions has no relation to any of these sites and has not been compensated
+        for their presence here. If you have concerns about these listings, contact us at <a href="mailto:fench@hauntedbees.com">fench@hauntedbees.com</a>.
     </p>
 </div>
 </template>
